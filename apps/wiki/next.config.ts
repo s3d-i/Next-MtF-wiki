e963bde6b02c3aaed8d1a511775ee7c3feeb12dd
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2025-06-14 19:44:43 +0800

    chore(deps): :fire: remove @cloudflare/next-on-pages

--- apps/wiki/next.config.ts
+++ apps/wiki/next.config.ts
@@ -1,10 +1,9 @@
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-import createMDX from "@next/mdx";
-import type { NextConfig } from "next";
+import createMDX from '@next/mdx';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: "export",
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  output: 'export',
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 };
 
 const withMDX = createMDX({
@@ -14,7 +13,4 @@ const withMDX = createMDX({
 export default withMDX(nextConfig);
 
 // configure runtime to edge for cloudflare pages
-export const runtime = "edge";
-if (process.env.NODE_ENV === "development") {
-  setupDevPlatform();
-}
+export const runtime = 'edge';
