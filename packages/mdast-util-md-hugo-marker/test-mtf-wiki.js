27cc3b91ae606cbb3196b97d0d7deb836b463deb
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2025-06-29 13:10:50 +0800

    chore: :art: improve turborepo cache, prettify print

--- packages/mdast-util-md-hugo-marker/test-mtf-wiki.js
+++ packages/mdast-util-md-hugo-marker/test-mtf-wiki.js
@@ -1,14 +1,18 @@
-import { hugoShortcodeFromMarkdown } from "./lib/index.js";
-import { fromMarkdown } from "mdast-util-from-markdown";
-import { hugoShortcode } from "micromark-extension-md-hugo-marker";
+import { formatWithOptions } from 'node:util';
+import { fromMarkdown } from 'mdast-util-from-markdown';
+import { hugoShortcode } from 'micromark-extension-md-hugo-marker';
+import { hugoShortcodeFromMarkdown } from './lib/index.js';
 
 const options = {
-  noSelfClosingElements: ['ref','mtf-wiki','telephone'],
-}
+  noSelfClosingElements: ['ref', 'mtf-wiki', 'telephone'],
+};
 
-const ast = fromMarkdown("{{< shields/qq 717099350 \"https://jq.qq.com/?_wv=1027&k=byC0cbS4\" />}}", {
-  extensions: [hugoShortcode()],
-  mdastExtensions: [hugoShortcodeFromMarkdown(options)],
-})
+const ast = fromMarkdown(
+  '{{< shields/qq 717099350 "https://jq.qq.com/?_wv=1027&k=byC0cbS4" />}}',
+  {
+    extensions: [hugoShortcode()],
+    mdastExtensions: [hugoShortcodeFromMarkdown(options)],
+  },
+);
 
-console.log(JSON.stringify(ast, null, 2))
+console.log(formatWithOptions({ colors: true, depth: null }, ast));
